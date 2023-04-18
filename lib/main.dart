43894593fb911b554291f9e5746cfab7a6540ac2
TeepaponChoak <teepapon.absolute@gmail.com>
2023-04-18 22:29:19 +0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              SizedBox(height: 130),
+              SizedBox(height: 200),
               Image.asset('assets/oceanpal_logo.png'),
               Text(
                   '      “your friend in\nocean conservation”',
