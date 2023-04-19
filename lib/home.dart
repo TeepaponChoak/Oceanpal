0dbb19a3e7b796bb782a3adb24a702216921b5e9
TeepaponChoak <teepapon.absolute@gmail.com>
2023-04-19 14:27:35 +0700

    Add rect

--- lib/home.dart
+++ lib/home.dart
@@ -5,13 +5,72 @@ class Mainpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          // Top Row
+          Container(
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 29,
+                  height: 29,
+                  margin: EdgeInsets.only(
+                    top: 40,
+                    left: 30,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: AssetImage('assets/menu.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 29,
+                  height: 29,
+                  margin: EdgeInsets.only(
+                    top: 40,
+                    right: 30,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: AssetImage('assets/noti.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+
+          // Rectangle with rounded bottom corners
+          Container(
+            margin: EdgeInsets.only(top: 25),
+            width: 344,
+            height: 183,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(31),
+                bottomRight: Radius.circular(31),
+                topLeft: Radius.circular(31),
+                topRight: Radius.circular(31),
+              ),
+              image: DecorationImage(
+                image: AssetImage('assets/turtle.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          // Other widgets in the column
+          // ...
+        ],
       ),
     );
   }
